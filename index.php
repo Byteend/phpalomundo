24c9f7511bbb6ec9af68293f49d42b011e04b62f
Bernardo Alves <51270900+Byteend@users.noreply.github.com>
2026-09-15 21:04:24 -0300

    Add index.php to display server date and time

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Data e Hora do Servidor</title>
+</head>
+<body>
+   <?php 
+        echo "bernardo alves de oliveira<br>";
+        echo date("y - m - d // h - i - s");       
+   ?>
+    
+</body>
+</html>
